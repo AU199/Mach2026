363c24db483684a43d5d740e48844e2d7b84829d
AU199 <akulgmvl@gmail.com>
2026-01-31 15:51:15 -0500

    done

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,15 @@
 
 package frc.robot;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -27,4 +36,15 @@ public final class Constants {
 
   public static final double IntakeDeployPos = 0;
   public static final double IntakeRetractPos = 0;
+  public static final String Path = Paths.get("").toAbsolutePath().toString()+"src\\\\main\\\\java\\\\frc\\\\robot\\\\AprilTags\\\\2026-rebuilt-welded.json";
+  public static AprilTagFieldLayout kTagLayout;
+  static{ 
+    try{
+      kTagLayout = new AprilTagFieldLayout(Path);
+  
+    }catch(IOException e){
+      System.out.println(e.getStackTrace());
+    }
+  }
+  public static final Transform3d kRobotToCam = new Transform3d(20, 0,20 ,new Rotation3d(0,0,0));
 }
